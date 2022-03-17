3d965ea45db7f862f2e550c64f2ba9dbe36cadf3
Chris Cheshire <2837753+chrift@users.noreply.github.com>
2022-03-17 21:15:36 +0000

    V0.0.1

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -1,13 +1,1 @@
-// import prodClient from './prod'
-//
-// let client = prodClient
-//
-// const run = async () => {
-//   if (process.env.NODE_ENV === 'development') {
-//     client = (await import('./dev')).default
-//   }
-// }
-//
-// run()
-
 export default process.env.NODE_ENV === 'production' ? require('./prod') : require('./dev')
